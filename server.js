4095122ff2ac96a558aa22b9ccd2ebbf7166e34d
Melissa Celeste Gries <m.c.gries@gmx.de>
2021-03-29 22:30:06 +0200

    add changes for heroku deployment

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
 const setupMongo = require('./setupMongo')
-
 require('dotenv').config()
+
 const { PORT = 4000 } = process.env
 
 setupMongo()
-
 const app = express()
 
-app.use(express.json())
-
-app.use(express.static('./client/build'))
-
+app.use('/', express.json())
 app.use('/api/cards', require('./routes/cards'))
-
+app.use(express.static('./client/build'))
 app.use(require('./routes/error'))
 
 app.listen(PORT, () => {
