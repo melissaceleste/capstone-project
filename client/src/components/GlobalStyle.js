3a9b61ff467c88a675b35bce5da2fd87ed2fa90e
melissaceleste <77294948+melissaceleste@users.noreply.github.com>
2021-03-30 12:09:25 +0200

    Update client/src/components/GlobalStyle.js
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>

--- client/src/components/GlobalStyle.js
+++ client/src/components/GlobalStyle.js
@@ -32,13 +32,3 @@ export default createGlobalStyle`
     }
   }
   `
-/* weiß
- background-color: #ADA996;
- background: -webkit-linear-gradient(to bottom, #EAEAEA, #DBDBDB, #F2F2F2, #ADA996);
- background: linear-gradient(to bottom, #EAEAEA, #DBDBDB, #F2F2F2, #ADA996); 
- 
- */
-
-/* lila background-color:  #D3CCE3;
-background: -webkit-linear-gradient(to bottom, #E9E4F0, #D3CCE3); 
-background: linear-gradient(to bottom, #E9E4F0, #D3CCE3); */
