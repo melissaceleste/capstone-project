d7029c0c3b38f52bc3948ff980a7d8fb51b00ff6
melissaceleste <77294948+melissaceleste@users.noreply.github.com>
2021-03-30 12:09:37 +0200

    Update client/src/services/createCard.js
    Co-authored-by: Lene <51355141+macfrei@users.noreply.github.com>

--- client/src/services/createCard.js
+++ client/src/services/createCard.js
@@ -2,7 +2,6 @@ export default function createCard(card) {
   return fetch('/api/cards', {
     method: 'POST',
     headers: {
-      // data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
       'content-type': 'application/json',
     },
     body: JSON.stringify(card),
